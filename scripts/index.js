922ab21c6443dcbb68696a7ae8dcdafbba8ca191
MartioloSky <martiolitozz@gmail.com>
2021-09-16 13:20:57 -0500

    trayendo los proximos-turnos y los turnos-activos a la sala de espera

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const textoDelMensaje = document.querySelector('.mensaje');
 const botonSpeech = document.querySelector('.boton-speech');
 const turnosActivos = document.querySelector('.turnos-activos');
 const proximosTurnos = document.querySelector('.contenedor-proximos-turnos');
+let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 var firebaseConfig = {
     apiKey: "AIzaSyASln_15XoqvOwSbCArUvhbe7laec_1r-Y",
@@ -22,15 +23,23 @@ botonSpeech.addEventListener("click", () => {
     window.speechSynthesis.speak(msg);
 })
 
+const fecha = new Date();
+const dia = fecha.getDate();
+const mes = fecha.getUTCMonth() + 1;
+const year = fecha.getFullYear();  
+const stringFecha = `${dia} ${meses[mes - 1]} ${year}`;
+
 database = firebase.database();
-var ref = database.ref('proximos-turnos');
-ref.on('value', gotData, errData);
+var referenciaProximosTurnos = database.ref(stringFecha + '/proximos-turnos');
+referenciaProximosTurnos.on('value', gotData, errData);
+
+var referenciaTurnosActivos = database.ref(stringFecha + '/turnos-activos');
+referenciaTurnosActivos.on('value', gotActivos, errData);
 
 function gotData(data) {
     borrarAnterioresProximosTurnos();
     var turnos = data.val();
     var keys = Object.keys(turnos);
-    console.log(keys);
 
     for (let i = 0; i < keys.length; i++) {
         var k = keys[i];
@@ -48,7 +57,34 @@ function gotData(data) {
         </div>
         `
 
-        proximosTurnos.appendChild(nuevoTurno)
+        proximosTurnos.appendChild(nuevoTurno);
+    }
+}
+
+function gotActivos(data) {
+    borrarAnterioresTurnosActivos();
+    var turnos = data.val();
+    var keys = Object.keys(turnos);
+
+    console.log(keys.length);
+
+    for (let i = 0; i < keys.length; i++) {
+        var k = keys[i];
+        var turno = turnos[k].turno;
+        var consultorio = turnos[k].consultorio;
+
+        const turnoActivo = document.createElement('div');
+        turnoActivo.classList.add('turno')
+        turnoActivo.innerHTML = `
+        <div class="contenedor-num-consultorio">
+            <h3 class="numero-consultorio">C-${consultorio}</h3>
+        </div>
+        <div class="contenedor-num-turno">
+            <h3 class="numero-turno">T-${turno}</h3>
+        </div>
+        `
+        
+        turnosActivos.appendChild(turnoActivo);
     }
 }
 
@@ -59,4 +95,17 @@ function errData(err) {
 
 function borrarAnterioresProximosTurnos() {
     proximosTurnos.innerHTML = ''
+}
+
+function borrarAnterioresTurnosActivos() {
+    turnosActivos.innerHTML = `
+    <div class="titulos-turnos-activos">
+        <div class="contenedor-titulo-consultorio">
+            <h3 class="numero-consultorio">Consultorio:</h3>
+        </div>
+        <div class="contenedor-titulo-turno">
+            <h3 class="numero-turno">Turno:</h3>
+        </div>
+    </div>
+    `
 }
